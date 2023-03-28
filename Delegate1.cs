e71eb4bb2156d0560f2e016dda3d07f098037128
Swaraa1211 <saiswaroopasaranath1211@gmail.com>
2023-03-28 15:12:25 +0530

    Add project files.

---
+++ Delegate1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    delegate void mathOperations(int a, int b);
+    internal class Delegate1
+    {
+        static void Main(string[] args)
+        {
+            mathOperations d1 = add;
+            mathOperations d2 = sub;
+            mathOperations d3 = mul;
+            mathOperations d4 = div;
+            mathOperations d5 = d1 + d2 + d3;
+
+            //Delegate[] arr = d5.GetInvocationList();
+            /*foreach(var item in arr)
+            {
+                Console.WriteLine(item);
+            }*/
+            d5.Invoke(5, 4);
+
+            //d5 -= d1;
+            d5 += d4;
+            d5.Invoke(3, 2);
+        }
+
+        static void add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        static void mul(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        static void div(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Delegates
+{
+    delegate void delegateForSum(int a, int b); //declaring delegate(signature)
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+
+            //int x = 10;
+            //int y = 20;
+            delegateForSum d1 = obj.Sum;
+            d1 += obj.Diff; //multicast delegate
+            d1(20, 50);
+
+            d1.Invoke(30, 60);
+
+            d1 -= obj.Diff;
+
+            d1(80, 70);
+            //Sum(x, y);
+        }
+        private void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void Diff(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+    }
+
+
+}
